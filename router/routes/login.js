77fc6b4b63780d4a1f0e8441223dc506bdad6533
dbswp <dbswp980427@gmail.com>
2023-03-22 11:32:54 +0900

    로그인, 회원가입 서비스 mogodb버전으로 변경

--- router/routes/login.js
+++ router/routes/login.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const userDb = require('../../controller/userController');
+const { loginUser } = require('../../controller/userController');
 
 const router = express.Router();
 
@@ -7,39 +7,41 @@ router.get('/', (req, res) => {
   res.render('login');
 });
 
-router.post('/', (req, res) => {
-  userDb.userCheck(req.body.id, (data) => {
-    if (data.length > 0) {
-      // 회원정보가 들어있는 data[0]
-      if (data[0].PASSWORD === req.body.password) {
-        // 백엔드 세션을 생성하는 코드
-        req.session.login = true;
-        req.session.userId = req.body.id;
+router.post('/', loginUser);
 
-        // 로그인 쿠키 발행
-        // user라는 이름에 회원정보 담기
-        res.cookie('user', req.body.id, {
-          maxAge: 1000 * 30,
-          httpOnly: true,
-          signed: true,
-        });
+// router.post('/', (req, res) => {
+//   userDb.userCheck(req.body.id, (data) => {
+//     if (data.length > 0) {
+//       // 회원정보가 들어있는 data[0]
+//       if (data[0].PASSWORD === req.body.password) {
+//         // 백엔드 세션을 생성하는 코드
+//         req.session.login = true;
+//         req.session.userId = req.body.id;
 
-        res.status(200);
-        res.redirect('/dbBoard');
-      } else {
-        res.status(400);
-        res.send(
-          '비밀번호가 다릅니다. <br><a href="/login">로그인 페이지로 이동</a>',
-        );
-      }
-    } else {
-      res.status(400);
-      res.send(
-        '해당 ID가 존재하지 않습니다. <br><a href="/login">로그인 페이지로 이동</a>',
-      );
-    }
-  });
-});
+//         // 로그인 쿠키 발행
+//         // user라는 이름에 회원정보 담기
+//         res.cookie('user', req.body.id, {
+//           maxAge: 1000 * 30,
+//           httpOnly: true,
+//           signed: true,
+//         });
+
+//         res.status(200);
+//         res.redirect('/dbBoard');
+//       } else {
+//         res.status(400);
+//         res.send(
+//           '비밀번호가 다릅니다. <br><a href="/login">로그인 페이지로 이동</a>',
+//         );
+//       }
+//     } else {
+//       res.status(400);
+//       res.send(
+//         '해당 ID가 존재하지 않습니다. <br><a href="/login">로그인 페이지로 이동</a>',
+//       );
+//     }
+//   });
+// });
 
 router.get('/logout', async (req, res) => {
   req.session.destroy((err) => {
--- router/routes/register.js
+++ router/routes/register.js
@@ -1,29 +1,34 @@
 const express = require('express');
-const userDb = require('../../controller/userController');
+const { registerUser } = require('../../controller/userController');
 
 const router = express.Router();
 
+// 회원가입 페이지로 이동
 router.get('/', (req, res) => {
   res.render('register');
 });
 
-router.post('/', async (req, res) => {
-  const duplicatedUser = await userDb.userCheck(req.body.id);
-  if (!duplicatedUser) {
-    const registerResult = await userDb.registerUser(req.body);
-    if (registerResult) {
-      res.status(200);
-      res.send('회원 가입 성공! </br> <a href="/login">로그인으로 이동</a>');
-    } else {
-      res.status(500);
-      res.send('회원 가입 실패! </br> <a href="/register">로그인으로 이동</a>');
-    }
-  } else {
-    res.status(400);
-    res.send(
-      '회원 가입 실패! 동일한 ID를 가진 회원이 존재합니다.</br> <a href="/register">회원가입으로 이동</a>',
-    );
-  }
-});
+router.post('/', registerUser);
 
+// 회원가입 기능 구현
+// router.post('/', async (req, res) => {
+//   const duplicatedUser = await userDb.userCheck(req.body.id);
+//   if (!duplicatedUser) {
+//     const registerResult = await userDb.registerUser(req.body);
+//     if (registerResult) {
+//       res.status(200);
+//       res.send('회원 가입 성공! </br> <a href="/login">로그인으로 이동</a>');
+//     } else {
+//       res.status(500);
+//       res.send('회원 가입 실패! </br> <a href="/register">로그인으로 이동</a>');
+//     }
+//   } else {
+//     res.status(400);
+//     res.send(
+//       '회원 가입 실패! 동일한 ID를 가진 회원이 존재합니다.</br> <a href="/register">회원가입으로 이동</a>',
+//     );
+//   }
+// });
+
+// 서버에서 사용
 module.exports = router;
