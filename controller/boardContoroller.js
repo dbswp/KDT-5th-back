a3ff23a3325a3a06b025d28295010cc915706df4
dbswp <dbswp980427@gmail.com>
2023-03-24 10:45:36 +0900

    서버 연결

--- controller/boardContoroller.js
+++ controller/boardContoroller.js
@@ -2,10 +2,10 @@
 const { ObjectId } = require('mongodb');
 const mongoClient = require('./mongoConnect');
 
-const mongooseConnect = require('./mongooseConnect');
+// const mongooseConnect = require('./mongooseConnect');
 // require('./mongooseConnect')
 // const로 변수를 받아오지 않아도 require로만으로도 불러올 수 있다.
-const Board = require('../models/board');
+// const board = require('../models/board');
 // mongooseConnect();
 
 const UNEXPECTED_MSG = '<br><a href="/">메인 페이지로 이동</a>';
@@ -18,7 +18,7 @@ const getAllArticles = async (req, res) => {
     const allArticleCursor = board.find({});
     const ARTICLE = await allArticleCursor.toArray();
 
-    res.render('db_Board', {
+    res.render('db_board', {
       ARTICLE,
       articleCounts: ARTICLE.length,
       userId: req.session.userId,
@@ -42,7 +42,7 @@ const writeArticle = async (req, res) => {
       CONTENT: req.body.content,
       IMAGE: req.file ? req.file.filename : null,
     };
-    await Board.create(newArticle);
+    await board.insertOne(newArticle);
     res.redirect('/dbBoard');
   } catch (err) {
     console.error(err);
@@ -54,7 +54,7 @@ const getArticles = async (req, res) => {
   try {
     const client = await mongoClient.connect();
     const board = client.db('kdt5').collection('board');
-    const selectedArticle = await Board.findOne({
+    const selectedArticle = await board.findOne({
       _id: ObjectId(req.params.id),
     });
     res.status(200);
@@ -76,7 +76,7 @@ const modifyArticle = async (req, res) => {
 
     if (req.file) modify.IMAGE = req.file.filename;
 
-    await Board.updateOne(
+    await board.updateOne(
       { _id: ObjectId(req.params.id) },
       {
         $set: modify,
@@ -94,7 +94,7 @@ const deleteArticles = async (req, res) => {
   try {
     const client = await mongoClient.connect();
     const board = client.db('kdt5').collection('board');
-    await Board.deleteOne({
+    await board.deleteOne({
       _id: ObjectId(req.params.id),
     });
     res.status(200).json('삭제성공');
@@ -111,61 +111,3 @@ module.exports = {
   modifyArticle,
   deleteArticles,
 };
-
-// const boardDB = {
-//   // 모든 게시글 가져오기
-//   getAllArticles: (cb) => {
-//     connection.query('SELECT * FROM mydb.board;', (err, data) => {
-//       if (err) throw err;
-//       console.log(data);
-//       cb(data);
-//     });
-//   },
-
-//   // 게시글 추가
-//   writeArticle: (newArticle, cb) => {
-//     connection.query(
-//       // controller에 저장되어있는 newArticle값 정확히 확인
-//       `INSERT INTO mydb.board (USERID, TITLE, CONTENT) VALUE ('${newArticle.id}','${newArticle.title}', '${newArticle.content}');`,
-//       (err, data) => {
-//         if (err) throw err;
-//         cb(data);
-//       },
-//     );
-//   },
-
-//   // 특정 id값을 가지는 게시글 찾기
-//   getArticles: (id, cb) => {
-//     connection.query(
-//       `SELECT * FROM mydb.board WHERE ID_PK = ${id};`,
-//       (err, data) => {
-//         if (err) throw err;
-//         cb(data);
-//       },
-//     );
-//   },
-
-//   // 특정 ID를 가지는 게시글 수정하는 컨트롤러
-//   modifyArticle: (id, modifyArticle, cb) => {
-//     connection.query(
-//       `UPDATE mydb.board SET TITLE = '${modifyArticle.title}', CONTENT = '${modifyArticle.content}' WHERE ID_PK = '${id}'`,
-//       (err, data) => {
-//         if (err) throw err;
-//         cb(data);
-//       },
-//     );
-//   },
-
-//   // 특정 ID를 가지는 게시글 삭제
-//   deleteArticle: (id, cb) => {
-//     connection.query(
-//       `DELETE FROM mydb.board WHERE ID_PK = ${id};`,
-//       (err, data) => {
-//         if (err) throw err;
-//         cb(data);
-//       },
-//     );
-//   },
-// };
-
-// module.exports = boardDB;
